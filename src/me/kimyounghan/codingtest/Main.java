851a782a8a98aaf738aa65dae27af771797046fb
KIMYOUNGHAN <yh22953905@gmail.com>
2020-09-09 21:07:50 +0900

    코딩 테스트 - 프로그래머스 -- 이분탐색 > 예산

--- src/me/kimyounghan/codingtest/Main.java
+++ src/me/kimyounghan/codingtest/Main.java
@@ -1,18 +1,15 @@
 package me.kimyounghan.codingtest;
 
-import me.kimyounghan.codingtest.programmers.DynamicProgramming.ExpressedByN;
-import me.kimyounghan.codingtest.programmers.dfsbfs.TargetNumber;
-import me.kimyounghan.codingtest.programmers.hash.APlayerWhoCouldNotFinish;
-import me.kimyounghan.codingtest.programmers.sort.LargestNumber;
-import me.kimyounghan.codingtest.programmers.stackandqueue.Top;
+
+import me.kimyounghan.codingtest.programmers.binarysearch.Budget;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ExpressedByN expressedByN = new ExpressedByN();
+        Budget budget = new Budget();
 
-        int answer = expressedByN.solution(5, 12);
+        int answer = budget.solution(new int[]{120, 110, 140, 150},485);
 
         System.out.println("answer : " + answer);
 
