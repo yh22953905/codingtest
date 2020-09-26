944ba46b8c9e88d7247e2a8cc1607a43c90a342a
KIMYOUNGHAN <yh22953905@gmail.com>
2020-09-26 17:26:07 +0900

    코딩 테스트 - 프로그래머스 -- 스택/큐 > 주식가격

--- src/me/kimyounghan/codingtest/Main.java
+++ src/me/kimyounghan/codingtest/Main.java
@@ -1,15 +1,15 @@
 package me.kimyounghan.codingtest;
 
 
-import me.kimyounghan.codingtest.programmers.stackandqueue.DevelopFuntion;
+import me.kimyounghan.codingtest.programmers.stackandqueue.StockPrice;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        DevelopFuntion developFuntion = new DevelopFuntion();
+        StockPrice developFuntion = new StockPrice();
 
-        int[] answer = developFuntion.solution(new int[]{80, 90, 0}, new int[]{1, 3, 30});
+        int[] answer = developFuntion.solution(new int[]{1, 2, 3, 2, 3});
 
         System.out.println("answer : " + answer);
 
