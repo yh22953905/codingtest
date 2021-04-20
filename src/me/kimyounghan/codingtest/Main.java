26018f16b7d4dfa86ea5347acc7f8f6923638b9c
aims-yhkim <yh.kim@aimskorea.kr>
2021-04-20 09:52:43 +0900

    코딩 테스트 - 프로그래머스 -- 정렬 > K번째수

--- src/me/kimyounghan/codingtest/Main.java
+++ src/me/kimyounghan/codingtest/Main.java
@@ -2,16 +2,23 @@ package me.kimyounghan.codingtest;
 
 
 import me.kimyounghan.codingtest.programmers.binarysearch.Immigration;
+import me.kimyounghan.codingtest.programmers.sort.KthNumber;
+
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Immigration immigration = new Immigration();
+        KthNumber kthNumber = new KthNumber();
 
-        long answer = immigration.solution(6, new int[]{7, 10});
+//        long answer = immigration.solution(6, new int[]{7, 10});
 
-        System.out.println("answer : " + answer);
+        int[] solution = kthNumber.solution(
+                new int[] {1, 5, 2, 6, 3, 7, 4}
+                , new int[][]{{2, 5, 3}, {4, 4, 1}, {1, 7, 3}});
+
+        System.out.println("answer : " + Arrays.stream(solution).asDoubleStream().toString());
 
     }
 
