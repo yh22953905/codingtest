b8ae852acb0e9db11318638438afa9341f48b6fd
aims-yhkim <yh.kim@aimskorea.kr>
2021-04-27 16:31:46 +0900

    코딩 테스트 - 프로그래머스 -- 힙 > 디스크 컨트롤러 1 (중간 커밋)

--- src/me/kimyounghan/codingtest/Main.java
+++ src/me/kimyounghan/codingtest/Main.java
@@ -1,14 +1,15 @@
 package me.kimyounghan.codingtest;
 
 
+import me.kimyounghan.codingtest.programmers.heap.DiskController;
 import me.kimyounghan.codingtest.programmers.heap.MoreSpicy;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        MoreSpicy moreSpicy = new MoreSpicy();
-        int solution = moreSpicy.solution(new int[]{1, 2, 3, 9, 10, 12}, 7);
+        DiskController diskController = new DiskController();
+        int solution = diskController.solution(new int[][]{{0, 3}, {1, 9}, {2, 6}});
 
         System.out.println("answer : " + solution);
 
