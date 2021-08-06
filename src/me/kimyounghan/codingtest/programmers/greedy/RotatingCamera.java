fd5f86e64e8c5e46bf09af77f24dc614adfe95bc
yh22953905 <yh22953905@gmail.com>
2021-08-06 20:19:19 +0900

    코딩 테스트 - 프로그래머스 -- 탐욕법 > 단속카메라 오류 수정

--- src/me/kimyounghan/codingtest/programmers/greedy/RotatingCamera.java
+++ src/me/kimyounghan/codingtest/programmers/greedy/RotatingCamera.java
@@ -9,25 +9,21 @@ public class RotatingCamera {
 	public int solution(int[][] routes) {
 		int answer = 0;
 
-		// 진입 지점 (routes[i][0])의 오름차순으로 정렬
-		Arrays.sort(routes, Comparator.comparingInt(o -> o[0]));
+		// 진입 지점 (routes[i][1])의 오름차순으로 정렬
+		Arrays.sort(routes, Comparator.comparingInt(o -> o[1]));
 
 		// 탐색, 각 경로의 진출 지점에 카메라 설치
 		// 이전 경로의 카메라에 찍히면 카메라를 설치하지 않음, 찍히지 않으면 카메라를 설치
-		Stack<Integer> cameraStack = new Stack<>();
-		cameraStack.push(-30001);
+		int camera = -30001;
 
-		for (int i = 0; i < routes.length; i++) {
-			if (cameraStack.peek() > routes[i][0] && cameraStack.peek() < routes[i][1]) {
-				cameraStack.push(routes[i][1]);
-			} else {
-				cameraStack.pop();
-				cameraStack.push(routes[i][1]);
+		for (int[] route : routes) {
+			if (camera < route[0]) {
+				camera = route[1];
+				answer++;
 			}
 		}
 
-		// 설치된 카메라의 개수가 answer
-		return cameraStack.size();
+		return answer;
 	}
 
 	public static void main(String[] args) {
