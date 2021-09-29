09173a9785e2e41a09b4038fec49a0a28b4c0727
yh22953905 <yh22953905@gmail.com>
2021-09-30 01:05:02 +0900

    코딩 테스트 - 프로그래머스 -- 탐욕법 > 섬 연결하기
    크루스칼 알고리즘을 사용하는 방식

--- src/me/kimyounghan/codingtest/programmers/greedy/ConnectingIsland.java
+++ src/me/kimyounghan/codingtest/programmers/greedy/ConnectingIsland.java
@@ -4,29 +4,32 @@ import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class ConnectingIsland {
 
+	private int[] islandArray;
+
 	/**
-	 * Programmers > 탐욕법 > 구명보트
+	 * Programmers > 탐욕법 > 섬 연결하기 
 	 *
 	 * 문제 설명
-	 * 무인도에 갇힌 사람들을 구명보트를 이용하여 구출하려고 합니다. 구명보트는 작아서 한 번에 최대 2명씩 밖에 탈 수 없고, 무게 제한도 있습니다.
-	 * 예를 들어, 사람들의 몸무게가 [70kg, 50kg, 80kg, 50kg]이고 구명보트의 무게 제한이 100kg이라면 2번째 사람과 4번째 사람은 같이 탈 수 있지만 1번째 사람과 3번째 사람의 무게의 합은 150kg이므로 구명보트의 무게 제한을 초과하여 같이 탈 수 없습니다.
-	 * 구명보트를 최대한 적게 사용하여 모든 사람을 구출하려고 합니다.
-	 * 사람들의 몸무게를 담은 배열 people과 구명보트의 무게 제한 limit가 매개변수로 주어질 때, 모든 사람을 구출하기 위해 필요한 구명보트 개수의 최솟값을 return 하도록 solution 함수를 작성해주세요.
+	 * n개의 섬 사이에 다리를 건설하는 비용(costs)이 주어질 때, 최소의 비용으로 모든 섬이 서로 통행 가능하도록 만들 때 필요한 최소 비용을 return 하도록 solution을 완성하세요.
+	 * 다리를 여러 번 건너더라도, 도달할 수만 있으면 통행 가능하다고 봅니다. 예를 들어 A 섬과 B 섬 사이에 다리가 있고, B 섬과 C 섬 사이에 다리가 있으면 A 섬과 C 섬은 서로 통행 가능합니다.
 	 *
 	 * 제한사항
-	 * 무인도에 갇힌 사람은 1명 이상 50,000명 이하입니다.
-	 * 각 사람의 몸무게는 40kg 이상 240kg 이하입니다.
-	 * 구명보트의 무게 제한은 40kg 이상 240kg 이하입니다.
-	 * 구명보트의 무게 제한은 항상 사람들의 몸무게 중 최댓값보다 크게 주어지므로 사람들을 구출할 수 없는 경우는 없습니다.
+	 * 섬의 개수 n은 1 이상 100 이하입니다.
+	 * costs의 길이는 ((n-1) * n) / 2이하입니다.
+	 * 임의의 i에 대해, costs[i][0] 와 costs[i] [1]에는 다리가 연결되는 두 섬의 번호가 들어있고, costs[i] [2]에는 이 두 섬을 연결하는 다리를 건설할 때 드는 비용입니다.
+	 * 같은 연결은 두 번 주어지지 않습니다. 또한 순서가 바뀌더라도 같은 연결로 봅니다. 즉 0과 1 사이를 연결하는 비용이 주어졌을 때, 1과 0의 비용이 주어지지 않습니다.
+	 * 모든 섬 사이의 다리 건설 비용이 주어지지 않습니다. 이 경우, 두 섬 사이의 건설이 불가능한 것으로 봅니다.
+	 * 연결할 수 없는 섬은 주어지지 않습니다.
 	 *
 	 * 입출력 예
-	 * people	limit	return
-	 * [70, 50, 80, 50]	100	3
-	 * [70, 80, 50]	100	3
+	 * n	costs	return
+	 * 4	[[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]	4
+	 *
+	 * 입출력 예 설명
+	 * costs를 그림으로 표현하면 다음과 같으며, 이때 초록색 경로로 연결하는 것이 가장 적은 비용으로 모두를 통행할 수 있도록 만드는 방법입니다.
 	 *
 	 * ------------------------------------------------------------------------------------
 	 *
@@ -62,9 +65,50 @@ public class ConnectingIsland {
 		return answer;
 	}
 
+	public int solution2(int n, int[][] costs) {
+		int answer = 0;
+
+		// costs[i][2]의 오름차순으로 정렬
+		Arrays.sort(costs, Comparator.comparingInt(o -> o[2]));
+
+		islandArray = new int[n];
+
+		// 섬 간 관계의 초기값
+		for (int i = 0; i < n; i++) {
+			islandArray[i] = i;
+		}
+
+		// 탐색, Minumum Spanning Tree 를 찾기 위한 크루스칼 알고리즘
+		for (int i = 0; i < costs.length; i++) {
+			int fromIsland = costs[i][0];
+			int toIsland = costs[i][1];
+			int cost = costs[i][2];
+
+			int fromParent = findParent(fromIsland);
+			int toParent = findParent(toIsland);
+
+			if (fromParent == toParent) continue;
+
+			answer = answer + cost;
+			islandArray[toIsland] = fromIsland;
+		}
+
+		return answer;
+	}
+
+	private int findParent(int island) {
+		if (islandArray[island] == island) {
+			// 노드의 부모가 자기 자신이라면 자기 자신을 리턴
+			return island;
+		}
+
+		return islandArray[island] = findParent(islandArray[island]);
+	}
+
 	public static void main(String[] args) {
 		ConnectingIsland connectingIsland = new ConnectingIsland();
-		int solution = connectingIsland.solution(4, new int[][] {{0,1,1}, {0,2,2}, {1,2,5}, {1,3,1}, {2,3,8}});
+		int solution = connectingIsland.solution2(4,
+			new int[][] {{0, 1, 1}, {0, 2, 2}, {1, 2, 5}, {1, 3, 1}, {2, 3, 8}});
 
 		System.out.println("answer : " + solution);
 	}
